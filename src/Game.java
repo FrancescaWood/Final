327ed8c4fbd647ca90e8b314925d84d850c0d06c
Francesca <s2106430@online.houstonisd.org>
2022-04-19 14:59:02 -0500

    add main and game

---
+++ src/Game.java
@@ -0,0 +1,171 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage; 
+import java.awt.event.*; 
+
+
+public class Game  extends JPanel implements Runnable, KeyListener, MouseListener, MouseMotionListener{
+
+	
+	private BufferedImage back; 
+	private int key, x, y; 
+	private ImageIcon background, bone, dog;
+	
+	
+	
+	public Game() {
+		new Thread(this).start();	
+		this.addKeyListener(this);
+		this.addMouseListener(this);
+		this.addMouseMotionListener(this);
+		key =-1; 
+		background = new ImageIcon("background2.jfif");
+		bone = new ImageIcon ("bone1-removebg-preview.png");
+		dog = new ImageIcon ("dog1-removebg-preview.png");
+		x=0;
+		y=0;
+		}
+
+	
+	
+	public void run()
+	   {
+	   	try
+	   	{
+	   		while(true)
+	   		{
+	   		   Thread.currentThread().sleep(5);
+	            repaint();
+	         }
+	      }
+	   		catch(Exception e)
+	      {
+	      }
+	  	}
+	
+
+	
+	
+	
+	public void paint(Graphics g){
+		
+		Graphics2D twoDgraph = (Graphics2D) g; 
+		if( back ==null)
+			back=(BufferedImage)( (createImage(getWidth(), getHeight()))); 
+		
+
+		Graphics g2d = back.createGraphics();
+	
+		g2d.clearRect(0,0,getSize().width, getSize().height);
+		g2d.drawImage(background.getImage(), 0, 0, getWidth(), getHeight(),this); 
+		g2d.drawImage(bone.getImage(), 530, 830, 150, 150,this); 
+		g2d.drawImage(dog.getImage(), x, y, 120, 120,this); 
+		
+		
+		Color myred = new Color(255,46,53);
+		g2d.setColor(myred);
+		g2d.setFont(new Font("TimesNewRoman",Font.BOLD, 50));
+		g2d.drawString("move the dog through the maze to reach his bone", 100, 100);
+
+		twoDgraph.drawImage(back, null, 0, 0);
+
+	}
+
+
+
+
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+		key= e.getKeyCode();
+		System.out.println(key);
+		
+		
+	}
+
+
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+	
+		
+		
+		
+	}
+
+
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		x=arg0.getX();
+		y=arg0.getY();
+	}
+
+
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+
+	
+}
