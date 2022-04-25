3199e4ec367ac05e2fffb783f48cb303065cba0f
Francesca <s2106430@online.houstonisd.org>
2022-04-25 15:02:39 -0500

    shark1, 2, 3

--- src/Game.java
+++ src/Game.java
@@ -10,6 +10,8 @@ public class Game  extends JPanel implements Runnable, KeyListener{
 	
 	private BufferedImage back; 
 	private int key; 
+	private ImageIcon shark11, shark22, shark33;
+	private Object shark1, shark2, shark3;
 	
 	//new
 	
@@ -17,6 +19,16 @@ public class Game  extends JPanel implements Runnable, KeyListener{
 		new Thread(this).start();	
 		this.addKeyListener(this);
 		key =-1; 
+		
+		shark1 = new Object();
+		shark2 = new Object();
+		shark3 = new Object();
+		
+		shark11 = new ImageIcon (shark1.getshark1());
+		shark22 = new ImageIcon (shark1.getshark2());
+		shark33 = new ImageIcon (shark1.getshark3());
+		
+		
 	}
 
 	
@@ -51,10 +63,14 @@ public class Game  extends JPanel implements Runnable, KeyListener{
 	
 		g2d.clearRect(0,0,getSize().width, getSize().height);
 		
-		
-		
-	
+		g2d.drawImage(shark11.getImage(), shark1.getX(), shark1.getY(), shark1.getH(), shark1.getW(), this);
+		g2d.drawImage(shark22.getImage(), shark2.getX(), shark2.getY(), shark2.getH(), shark2.getW(), this);
+		g2d.drawImage(shark33.getImage(), shark3.getX(), shark3.getY(), shark3.getH(), shark3.getW(), this);
 
+		shark1.moveShark1();
+		shark2.moveShark2();
+		shark3.moveShark3();
+		
 		twoDgraph.drawImage(back, null, 0, 0);
 
 	}
--- src/Object.java
+++ src/Object.java
@@ -1,4 +1,95 @@
 
 public class Object {
 
+	private int x,y,dx,dy,w,h;
+	private String shark1, shark2, shark3, shark4, shark5, shark6;
+	
+	
+	public Object() {
+		x=600;
+		y=450;
+		w=150;
+		h=150;
+		dx=1;
+		dy=1;
+		shark1 = "OIP__1_-removebg-preview.png";
+		shark2 = "iceboy-removebg-preview.png";
+		shark3 = "ice-skating-clipart-10-removebg-preview.png";
+		
+	}
+	
+	public Object(int xV, int yV, int dxV, int dyV) {
+		x=xV;
+		y=yV;
+		w=150;
+		h=150;
+		dx=dxV;
+		dy=dyV;
+		shark1 = "OIP__1_-removebg-preview.png";
+		shark2 = "iceboy-removebg-preview.png";
+		shark3 = "ice-skating-clipart-10-removebg-preview.png";
+		
+		
+		
+	}
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public int getW() {
+		return w;
+	}
+	public int getH() {
+		return h;
+	}
+	public String getshark1() {
+		return shark1;
+	}
+	public String getshark2() {
+		return shark2;
+	}
+	public String getshark3() {
+		return shark3;
+	}
+	
+	public void setX(int xV) {
+		x=xV;
+	}
+	public void moveShark1() {
+		x+=dx;
+		y+=dy;
+		//System.out.println("x=" + x);
+		
+		if (x >= 673 || x <=550) {
+			dx*= -1;
+		}
+		if (y >= 480 || y <= 380) {
+			dy*= -1; 
+	}}
+	public void moveShark2() {
+		x+=dx;
+		y+=dy;
+		//System.out.println("x=" + x);
+		
+		if (x >= 773 || x <=550) {
+			dx*= -1;
+		}
+		if (y >= 480 || y <= 400) {
+			dy*= -1; 
+	}}
+		public void moveShark3() {
+			x+=dx;
+			y+=dy;
+			//System.out.println("x=" + x);
+			
+			if (x <= 0 || x >=100) {
+				dx*= -1;
+			}
+			if (y >= 600 || y <= 480) {
+				dy*= -1; 
+		}
+	}
+	
 }
